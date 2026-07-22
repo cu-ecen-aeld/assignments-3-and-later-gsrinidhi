962a2353ebf08c77e8f3567d93b2611483c9ac7e
Srinidhi <gonahalsrinidhi@gmail.com>
2026-07-22 20:43:11 +0530

    Commit after fixing aesdsocket path mistake

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,3 +1,12 @@
 AESD_SOCKET_PATH=$(pwd)/aesdsocket
+hname=$(hostname)
+if [ ${hname} = "buildroot" ]
+then
+    echo "Running from buildroot"
+	AESD_SOCKET_PATH=/usr/bin/aesdsocket
+else
+    echo "Running from host machine"
+	AESD_SOCKET_PATH=$(pwd)/aesdsocket
+fi
 echo "aesdsocket path = ${AESD_SOCKET_PATH}"
 start-stop-daemon --start --exec ${AESD_SOCKET_PATH} -- -d
